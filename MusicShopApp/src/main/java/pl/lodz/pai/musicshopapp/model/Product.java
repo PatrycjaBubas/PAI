94f554b2f9a80b799ec7b50eacf2cec57537157c
calapawel <cala.pawel.93@gmail.com>
2018-01-23 21:00:03 +0100

    Aplikacja - backend

---
+++ MusicShopApp/src/main/java/pl/lodz/pai/musicshopapp/model/Product.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.lodz.pai.musicshopapp.model;
+
+/**
+ *
+ * @author Paweł
+ */
+public class Product {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    private String name;
+    private double amount;
+    private double price;
+    private String description;
+    
+    
+}
